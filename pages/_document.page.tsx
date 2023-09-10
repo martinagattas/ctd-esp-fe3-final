4273f7cda17e4360e081861958fc75a1a1aecfb5
“martina <martina@goschool.com.ar>
2023-09-10 10:33:03 -0300

    initial settings

--- pages/_document.page.tsx
+++ pages/_document.page.tsx
@@ -1,4 +1,4 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
     return (
@@ -8,12 +8,12 @@ export default function Document() {
                     href="https://fonts.googleapis.com/css2?family=Roboto"
                     rel="stylesheet"
                 />
+                <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
             </Head>
             <body style={{height: '100%'}}>
-            <Main />
-            <NextScript />
+                <Main />
+                <NextScript />
             </body>
-
         </Html>
     )
 }
